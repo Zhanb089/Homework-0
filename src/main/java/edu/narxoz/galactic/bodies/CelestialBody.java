f2bdc6968d99b37b554efc248f285e5ab93be378
Zhanat Zhanbolat <janat.janbolat2000@gmail.com>
2026-01-30 19:25:14 +0500

    Second commit

---
+++ src/main/java/edu/narxoz/galactic/bodies/CelestialBody.java
@@ -0,0 +1,5 @@
+package main.java.edu.narxoz.galactic.bodies;
+
+public class CelestialBody {
+    
+}
---
+++ src/main/java/edu/narxoz/galactic/drones/Drone.java
@@ -0,0 +1,5 @@
+package main.java.edu.narxoz.galactic.drones;
+
+public class Drone {
+    
+}
